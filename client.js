38e9bb0ddda8a16aca36bf7f756a70276cb0ca5a
Alessandro Re <ale@ale-re.net>
2019-12-01 23:52:23 +0100

    Added delete button and code prettified

--- client.js
+++ client.js
@@ -1,14 +1,17 @@
-'use strict';
+"use strict";
 
-var sharedb = require('sharedb/lib/client');
-var Cookies = require('js-cookie');
+var sharedb = require("sharedb/lib/client");
+var Cookies = require("js-cookie");
 
 function generateRandomUID() {
     return new Array(64)
         .fill(0)
-        .map(x => String
-            .fromCharCode('A'.charCodeAt(0) + Math.floor(Math.random() * 26)))
-        .join('');
+        .map(x =>
+            String.fromCharCode(
+                "A".charCodeAt(0) + Math.floor(Math.random() * 26)
+            )
+        )
+        .join("");
 }
 
 function getWebsocketURI() {
@@ -18,21 +21,21 @@ function getWebsocketURI() {
         var search = "";
         if (document.cookie) {
             console.log("Empty location.search, rebuilding search");
-            search = '?uid=' + Cookies.get('uid');
+            search = "?uid=" + Cookies.get("uid");
         } else {
             console.log("Empty location.search, building new one");
             var uid = generateRandomUID();
             // Store the uid as a cookie
-            Cookies.set('uid', uid);
+            Cookies.set("uid", uid);
             // Since search is not present, we create a random token to access as anon
-            search = '?uid=' + uid;
+            search = "?uid=" + uid;
         }
         console.log("Search is now", window.location.host, search);
         window.location.search = search;
     } else {
         console.log("Here's the search", window.location.search);
     }
-    return 'ws://' + window.location.host + window.location.search;
+    return "ws://" + window.location.host + window.location.search;
 }
 
 var authSocket = new WebSocket(getWebsocketURI());
@@ -40,128 +43,157 @@ var authSocket = new WebSocket(getWebsocketURI());
 authSocket.onclose = function(ev) {
     console.log(ev);
     if (ev.code == 3000) {
-        document.getElementById('messages').innerText = "Permission denied :(";
+        document.getElementById("messages").innerText = "Permission denied :(";
     } else {
-        document.getElementById('messages').innerText = "Connection closed!";
+        document.getElementById("messages").innerText = "Connection closed!";
     }
 };
 
 authSocket.onmessage = function(ev) {
     var token = ev.data;
     var config = JSON.parse(ev.data);
-    console.log('Got config', config);
+    console.log("Got config", config);
 
     // Auth ok: set-up page
-    document.getElementById('checklist-form').style.display = 'block';
+    document.getElementById("checklist-form").style.display = "block";
 
     // Some people are authorized to add things
     if (config.canAdd === true) {
-        var input = document.querySelector('#checklist-form>input[type="text"]');
-        input.style.display = 'block';
-        input.addEventListener('keydown', function(event) {
-            if (event.key == 'Enter') {
+        var input = document.querySelector(
+            '#checklist-form>input[type="text"]'
+        );
+        input.style.display = "block";
+        input.addEventListener("keydown", function(event) {
+            if (event.key == "Enter") {
                 event.preventDefault();
                 var label = event.target.value;
-                console.log('Adding new checkbox with value ' + label);
-                console.log('Data status: ' + doc.data.labels.length + ', ' + doc.data.calls.length);
+                console.log("Adding new checkbox with value " + label);
+                console.log(
+                    "Data status: " +
+                        doc.data.labels.length +
+                        ", " +
+                        doc.data.calls.length
+                );
                 doc.submitOp([
-                    {p: ['labels', 0], li: label},
-                    {p: ['calls', 0], li: false},
-                    {p: ['owner', 0], li: ''},
+                    { p: ["labels", 0], li: label },
+                    { p: ["calls", 0], li: false },
+                    { p: ["owner", 0], li: "" }
                 ]);
-                console.log('Data status: ' + doc.data.labels.length + ', ' + doc.data.calls.length);
-                event.target.value = '';
+                console.log(
+                    "Data status: " +
+                        doc.data.labels.length +
+                        ", " +
+                        doc.data.calls.length
+                );
+                event.target.value = "";
+            }
+        });
+    }
+    // Other people are allowed to remove things
+    if (config.canDel === true) {
+        var e_checks = document.querySelector(".checkboxes");
+        e_checks.addEventListener("click", function(event) {
+            if (event.target.className.indexOf("delete") >= 0) {
+                event.preventDefault();
+                // Find position of child
+                var child = event.target.parentNode.parentNode;
+                var i = 0;
+                while ((child = child.previousSibling) != null) i++;
+                doc.submitOp([
+                    { p: ["labels", i], ld: doc.data.labels[i] },
+                    { p: ["calls", i], ld: doc.data.calls[i] },
+                    { p: ["owner", i], ld: doc.data.owner[i] }
+                ]);
             }
         });
     }
 
     var enabler = function(owner, j) {
         return function() {
-            console.log('Changing checkbox ' + j, ': ', this, this.checked);
+            console.log("Changing checkbox " + j, ": ", this, this.checked);
             // Set my owner if I'm checking, else set it free
-            var newOwner = this.checked ? config.token : '';
+            var newOwner = this.checked ? config.token : "";
             doc.submitOp([
-                {p: ['calls', j], ld: !this.checked, li: this.checked},
-                {p: ['owner', j], ld: owner, li: newOwner},
+                { p: ["calls", j], ld: !this.checked, li: this.checked },
+                { p: ["owner", j], ld: owner, li: newOwner }
             ]);
             return false;
         };
     };
-    // global.enabler = enabler; // FIXME remove this, not necessary
 
-    var uri = 'ws://' + window.location.host + window.location.search + '&token=' + config.token;
-    /*
-    // Manage anon users (no UID specified, no location.search)
-    var uri = 'ws://' + window.location.host;
-    if (window.location.search) {
-        console.log("Window location search: " + window.location.search);
-        uri += window.location.search + '&token=' + config.token;
-    } else {
-        // Since search is not present, we create a random token to access as anon
-        console.log("Window location search: " + window.location.search);
-        var ruid = new Array(64)
-            .fill(0)
-            .map(x => String
-                .fromCharCode('A'.charCodeAt(0) + Math.floor(Math.random() * 26)))
-            .join('');
-        uri += '/?uid=' + ruid + '&token=' + config.token;
-        console.log("Created random uid", uri);
-    }
-    */
+    var uri =
+        "ws://" +
+        window.location.host +
+        window.location.search +
+        "&token=" +
+        config.token;
 
-    console.log('Opening connection to ' + uri);
+    console.log("Opening connection to " + uri);
     // Open WebSocket connection to ShareDB server
     var socket = new WebSocket(uri);
     var connection = new sharedb.Connection(socket);
 
     // Create local doc instance
-    var doc = connection.get('collection', 'my-checklist');
+    var doc = connection.get("collection", "my-checklist");
 
     // Get initial value of document and subscribe to changes
     doc.subscribe(showNumbers);
     // When document changes (by this client or any other, or the server),
     // update the number on the page
-    doc.on('op', showNumbers);
+    doc.on("op", showNumbers);
 
     function showNumbers() {
-        console.log('UPDATE! Data status: ' + doc.data.labels + ', ' + doc.data.calls);
-        var e_checks = document.querySelector('.checkboxes');
+        console.log(
+            "UPDATE! Data status: " + doc.data.labels + ", " + doc.data.calls
+        );
+        var e_checks = document.querySelector(".checkboxes");
         if (doc.data.labels.length != doc.data.calls.length) {
-            console.log('Ignoring update due to missing data! labels-vs-calls: ' + doc.data.labels.length + ' ' + doc.data.calls.length);
+            console.log(
+                "Ignoring update due to missing data! labels-vs-calls: " +
+                    doc.data.labels.length +
+                    " " +
+                    doc.data.calls.length
+            );
             return; // Do not process this change, something is being added
         }
-        e_checks.innerHTML = '';
-        console.log('Processing update, got', doc.data.labels.length, 'elements');
+        e_checks.innerHTML = "";
+        console.log(
+            "Processing update, got",
+            doc.data.labels.length,
+            "elements"
+        );
         for (var i = 0; i < doc.data.labels.length; i++) {
             var state = doc.data.calls[i];
             var label = doc.data.labels[i];
             var owner = doc.data.owner[i];
-            var e_li = document.createElement('li');
-            var e_label = document.createElement('label');
-            var e_box = document.createElement('input');
-            var e_span = document.createElement('span');
-            e_box.setAttribute('name', 'item');
-            e_box.setAttribute('type', 'checkbox');
+            var e_li = document.createElement("li");
+            var e_label = document.createElement("label");
+            var e_box = document.createElement("input");
+            var e_span = document.createElement("span");
+            e_box.setAttribute("name", "item");
+            e_box.setAttribute("type", "checkbox");
             e_box.checked = state;
-            e_span.innerText = label;
+            e_span.innerHTML = label;
             e_label.appendChild(e_box);
             e_label.appendChild(e_span);
+            if (config.canDel === true) {
+                var e_del = document.createElement("span");
+                e_del.innerText = " \u274c";
+                e_del.className = "delete";
+                e_label.appendChild(e_del);
+            }
             e_li.appendChild(e_label);
             e_checks.appendChild(e_li);
 
-            console.log('Got checkbox "' + label + '"' + ' with owner "' + owner + '"');
+            console.log(
+                'Got checkbox "' + label + '"' + ' with owner "' + owner + '"'
+            );
 
-            if (owner != '' && owner != config.token) {
-                e_box.setAttribute('disabled', true);
+            if (owner != "" && owner != config.token) {
+                e_box.setAttribute("disabled", true);
             } else {
-                var en = enabler(owner, i);
-                global.enablers.push(en); // FIXME remove this test function
-                e_box.addEventListener('change', en);
+                e_box.addEventListener("change", enabler(owner, i));
             }
         }
-    };
-
-    // Expose to index.html
-    //global.addCheckbox = addCheckbox;
-    global.enablers = []; // FIXME remove this test function
+    }
 };
--- server.js
+++ server.js
@@ -1,24 +1,26 @@
-var _ = require('underscore');
-var fs = require('fs');
-var crypto = require('crypto');
+var _ = require("underscore");
+var fs = require("fs");
+var crypto = require("crypto");
 
-var http = require('http');  // Creating http server
-var express = require('express');  // Web framework
-var url = require('url');
-var querystring = require('querystring');
-var WebSocket = require('ws');  // Realtime communication
-var WebSocketJSONStream = require('websocket-json-stream');
+var http = require("http"); // Creating http server
+var express = require("express"); // Web framework
+var url = require("url");
+var querystring = require("querystring");
+var WebSocket = require("ws"); // Realtime communication
+var WebSocketJSONStream = require("websocket-json-stream");
 
-var ShareDB = require('sharedb');  // The magic is here :)
-var ShareDBAccess = require('sharedb-access');
-var db = require('sharedb-mongo')('mongodb://banana:ananas@localhost:27017/leest');
+var ShareDB = require("sharedb"); // The magic is here :)
+var ShareDBAccess = require("sharedb-access");
+var db = require("sharedb-mongo")(
+    "mongodb://banana:ananas@localhost:27017/leest"
+);
 
 // This parse the acess control file and builds easy to use structures
 class Auth {
     constructor(path) {
         console.log("Parsing authorization file");
         //th`is.path = path;
-        var list = JSON.parse(fs.readFileSync(path, 'utf8'));
+        var list = JSON.parse(fs.readFileSync(path, "utf8"));
 
         // Sets of users that have certain permissions
         this.readers = Object.create(null);
@@ -29,38 +31,36 @@ class Auth {
 
         for (var i in list) {
             console.log("Parsing", list[i]);
-            if (!('uid' in list[i])) {
+            if (!("uid" in list[i])) {
                 console.log("Missing UID key, skipping auth entry", i, list[i]);
                 continue;
             }
-            var uid = list[i]['uid'];
+            var uid = list[i]["uid"];
             this.uids[uid] = true; // Store valid UIDs
             // Anon might allow empty uid or not
             if (uid === null) {
-                this.allowEmptyUID = list[i]['allowEmpty'] || false;
-                console.log("Allowing empty UIDs for anon:", this.allowEmptyUID);
+                this.allowEmptyUID = list[i]["allowEmpty"] || false;
+                console.log(
+                    "Allowing empty UIDs for anon:",
+                    this.allowEmptyUID
+                );
             }
 
             var access = "m"; // Marker by default
-            if ('access' in list[i]) {
-                access = list[i]['access'];
+            if ("access" in list[i]) {
+                access = list[i]["access"];
             }
-            if (access.includes('r'))
-                this.readers[uid] = true;
-            if (access.includes('m'))
-                this.markers[uid] = true;
-            if (access.includes('a'))
-                this.adders[uid] = true;
-            if (access.includes('d'))
-                this.deleters[uid] = true;
+            if (access.includes("r")) this.readers[uid] = true;
+            if (access.includes("m")) this.markers[uid] = true;
+            if (access.includes("a")) this.adders[uid] = true;
+            if (access.includes("d")) this.deleters[uid] = true;
         }
     }
 
     // any UID which was not present in the access file is considered "anon"
     toValidUID(uid) {
         // If uid was in the access file, then is valid
-        if (uid in this.uids)
-            return uid;
+        if (uid in this.uids) return uid;
         // If was not present, then we consider it as "anon"
         // as anonymous users will have randomly assegnated UIDs
         return null;
@@ -87,26 +87,35 @@ class Auth {
 
     hasFullAccess(uid) {
         uid = this.toValidUID(uid);
-        return this.canRead(uid) && this.canMark(uid) && this.canAdd(uid) && this.canDelete(uid);
+        return (
+            this.canRead(uid) &&
+            this.canMark(uid) &&
+            this.canAdd(uid) &&
+            this.canDelete(uid)
+        );
     }
 
     makeToken(uid) {
-        if (uid === null)
-            return "00000000000000000000000000000000"; // Anon token
-        return crypto.createHash('md5').update(uid).digest('hex');
+        if (uid === null) return "00000000000000000000000000000000"; // Anon token
+        return crypto
+            .createHash("md5")
+            .update(uid)
+            .digest("hex");
     }
 }
 
 var auth = new Auth("allowed.json");
 
-var collectionName = 'collection';
-var backend = new ShareDB({db});
+var collectionName = "collection";
+var backend = new ShareDB({ db });
 
-backend.use('connect', (request, next) => {
-    console.log('Connection');
+backend.use("connect", (request, next) => {
+    console.log("Connection");
     if (!_.isUndefined(request.req)) {
-        request.agent.connectSession = {'uid': request.req.uid,
-                                        'token': request.req.token};
+        request.agent.connectSession = {
+            uid: request.req.uid,
+            token: request.req.token
+        };
     }
     next();
 });
@@ -128,59 +137,77 @@ backend.allowCreate(collectionName, async (docId, doc, session) => {
 
 // Now check if the user is allowed to update the document.
 // Some users can add fields, other users can only change check status
-backend.allowUpdate(collectionName, async (docId, oldDoc, newDoc, ops, session) => {
-    console.log("Update", docId, session);
-    if (auth.hasFullAccess(session.uid)) {
-        console.log("User " + session.uid + " allowed to change anything");
+backend.allowUpdate(
+    collectionName,
+    async (docId, oldDoc, newDoc, ops, session) => {
+        console.log("Update", docId, session);
+        if (auth.hasFullAccess(session.uid)) {
+            console.log("User " + session.uid + " allowed to change anything");
+            return true;
+        }
+
+        if (oldDoc.labels.length != newDoc.labels.length) {
+            console.log("User " + session.uid + " cannot change the labels!");
+            return false;
+        }
+
+        if (oldDoc.calls.length != newDoc.calls.length) {
+            console.log("User " + session.uid + " cannot change the calls!");
+            return false;
+        }
+
+        if (oldDoc.owner.length != newDoc.owner.length) {
+            console.log("User " + session.uid + " cannot change the owner!");
+            return false;
+        }
+
+        // Check ops if there is a change of owner
+        for (var i = 0; i < ops.length; i++) {
+            console.log("  Update op", ops[i]);
+            if (ops[i]["p"][0] != "owner") continue;
+            var op = ops[i]["p"];
+            var oldOwner = oldDoc.owner[op[1]];
+            var newOwner = newDoc.owner[op[1]];
+            console.log(
+                '    Changing owner from "' +
+                    oldOwner +
+                    '" to "' +
+                    newOwner +
+                    '" by user "' +
+                    session.uid +
+                    '" "' +
+                    session.token +
+                    '"'
+            );
+            if (oldOwner != "" && oldOwner != session.token) {
+                console.log(
+                    " -> Permission denied, old owner is not null and not myself"
+                );
+                return false; // Cannot change another owner!
+            }
+        }
+        console.log(
+            "Update: user",
+            session.uid,
+            "managed to update " + oldDoc + " to " + newDoc
+        );
+
         return true;
     }
-
-    if (oldDoc.labels.length != newDoc.labels.length) {
-        console.log("User " + session.uid + " cannot change the labels!");
-        return false;
-    }
-
-    if (oldDoc.calls.length != newDoc.calls.length) {
-        console.log("User " + session.uid + " cannot change the calls!");
-        return false;
-    }
-
-    if (oldDoc.owner.length != newDoc.owner.length) {
-        console.log("User " + session.uid + " cannot change the owner!");
-        return false;
-    }
-
-    // Check ops if there is a change of owner
-    for (var i = 0; i < ops.length; i++) {
-        console.log("  Update op", ops[i]);
-        if (ops[i]['p'][0] != 'owner')
-            continue;
-        var op = ops[i]['p'];
-        var oldOwner = oldDoc.owner[op[1]];
-        var newOwner = newDoc.owner[op[1]];
-        console.log('    Changing owner from "' + oldOwner + '" to "' + newOwner + '" by user "' + session.uid + '" "' + session.token + '"');
-        if (oldOwner != '' && oldOwner != session.token) {
-            console.log(' -> Permission denied, old owner is not null and not myself');
-            return false; // Cannot change another owner!
-        }
-    }
-    console.log("Update: user", session.uid, "managed to update " + oldDoc + " to " + newDoc);
-
-    return true;
-});
+);
 
 // Create initial document then fire callback
 function createDoc(callback) {
-  var connection = backend.connect();
-  var doc = connection.get(collectionName, 'my-checklist');
-  doc.fetch(function(err) {
-    if (err) throw err;
-    if (doc.type === null) {
-      doc.create({labels: [], calls: [], owner: []}, callback);
-      return;
-    }
-    callback();
-  });
+    var connection = backend.connect();
+    var doc = connection.get(collectionName, "my-checklist");
+    doc.fetch(function(err) {
+        if (err) throw err;
+        if (doc.type === null) {
+            doc.create({ labels: [], calls: [], owner: [] }, callback);
+            return;
+        }
+        callback();
+    });
 }
 
 // Il server deve iniziare la sessione sapendo che UID e token sono correlati
@@ -189,37 +216,14 @@ function createDoc(callback) {
 function startServer() {
     // Create a web server to serve files and listen to WebSocket connections
     var app = express();
-    app.use(express.static('static'));
-    app.get('/', (req, res) => {
-        res.send(`
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="style.css" />
-    </head>
-    <body>
-        <section>
-            <h2>Checklist</h2>
-            <p id='messages'></p>
-            <form id="checklist-form" style='display: none;'>
-                <input type='text' placeholder='Add element' style='display: none;' />
-                <ul class="checkboxes">
-                </ul>
-            </form>
-        </section>
-        <script src="dist/bundle.js"></script>
-    </body>
-</html>`);
-    });
+    app.use(express.static("static"));
     var server = http.createServer(app);
 
     // Connect any incoming WebSocket connection to ShareDB
-    var wss = new WebSocket.Server({server: server});
-    wss.on('connection', function(ws, req) {
+    var wss = new WebSocket.Server({ server: server });
+    wss.on("connection", function(ws, req) {
         var query = querystring.parse(url.parse(req.url).query);
-        console.log('Started connection from ' + req.url);
+        console.log("Started connection from " + req.url);
         console.log("Got query params", query);
 
         var uid = query.uid;
@@ -231,7 +235,7 @@ function startServer() {
                 uid = null; // No token, anon user
             } else {
                 // We don't allow empty UIDs
-                ws.close(3000, 'Not allowed');
+                ws.close(3000, "Not allowed");
                 return;
             }
         }
@@ -241,20 +245,23 @@ function startServer() {
             // If token is undefined, connection has to be established
             if (auth.canRead(uid)) {
                 console.log("UID can read, connection from uid " + uid);
-                ws.send(JSON.stringify({
-                    'token': auth.makeToken(uid),  // Send access token to client
-                    'canAdd': auth.canAdd(uid),
-                }));
+                ws.send(
+                    JSON.stringify({
+                        token: auth.makeToken(uid), // Send access token to client
+                        canAdd: auth.canAdd(uid),
+                        canDel: auth.canDelete(uid)
+                    })
+                );
             } else {
                 // User cannot read, not allowed
-                ws.close(3000, 'Not allowed');
+                ws.close(3000, "Not allowed");
             }
         } else {
             // We got both UID and token, we can open a connection
             if (auth.makeToken(uid) == token) {
                 console.log("Started auth connection with token " + token);
                 var stream = new WebSocketJSONStream(ws);
-                backend.listen(stream, {'uid': uid, 'token': token});
+                backend.listen(stream, { uid: uid, token: token });
             } else {
                 console.log("Wrong token!"); // FIXME shall we give back some error code here?
             }
@@ -267,4 +274,3 @@ function startServer() {
 }
 
 createDoc(startServer);
-
